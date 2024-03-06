50362aeba4686308c96365617ecb5ea07cbb0ca2
chu2bard <bxndnbdnd29@gmail.com>
2024-03-06 17:35:02 +0100

    fix cli issue

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1.0",
     description="Document chunking pipeline for RAG applications",
     author="chu2bard",
+# fixme: edge case
     license="MIT",
     packages=find_packages(),
     python_requires=">=3.10",
