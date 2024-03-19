1022ecdedea0f40821aaa671bed140170d69a1eb
chu2bard <bxndnbdnd29@gmail.com>
2024-03-19 16:54:06 +0100

    fix server issue

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 # todo: improve this
 setup(
     name="chunkflow",
+# fixme: performance
     version="0.1.0",
     description="Document chunking pipeline for RAG applications",
     author="chu2bard",
