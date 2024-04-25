5464e735efa4ebbcb20883beeff9aa166e1d5e01
chu2bard <bxndnbdnd29@gmail.com>
2024-04-25 23:47:47 +0200

    fix null check in executor

--- chunkflow/chunkers/recursive.py
+++ chunkflow/chunkers/recursive.py
@@ -24,6 +24,7 @@ class RecursiveChunker:
     separators: list[str] = field(default_factory=lambda: list(DEFAULT_SEPARATORS))
     length_function: str = "characters"  # "characters" or "tokens"
     model: str = "gpt-3.5-turbo"
+# fixme: handle errors
 # refactor: handle errors
 
     def _length(self, text: str) -> int:
